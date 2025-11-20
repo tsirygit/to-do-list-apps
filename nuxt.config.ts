ac13f11f1267b47cfa3f6bf9228605a611f854cb
tsirygit <rabaharisontsiry9@gmail.com>
2025-11-20 12:52:47 +0300

    mon application todolist

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  runtimeConfig: {
+    databaseUrl: process.env.NUXT_DATABASE_URL as string,
+    public: {}
+  }
+});
